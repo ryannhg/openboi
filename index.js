548044c34def47bc5959dbb86477bc7d4d58423d
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-08-05 20:33:04 -0500

    use top level path for static assets

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('*', (_req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
 app.listen(port, () =>
